327114d71382ccfdb0a29ecbea19df33b419278e
Ola Ishola <olaishola@hotmail.co.uk>
2020-03-10 23:33:53 +0100

    gaming working as expected but needs few adjustment

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const randNum = Math.floor(Math.random() * (11 - 1)) + 1;
+let counter = 0;
+let maxGuess = 5;
 
-const getBtn = document.getElementsByTagName("button").onclick(function () {
-    const getInput = document.getElementsByTagName("input").value;
-    if (getInput == randNum) {
-        alert("Hooray, You guessed right");
+
+const getBtn = document.getElementById("getBttn").onclick = function () {
+    const getInput = document.getElementById("getInput").value;
+
+    if (counter < maxGuess) {
+        if (getInput > 10) {
+            alert("the number u entered is too large");
+        } if (getInput == randNum) {
+            alert("Yes! it took you " + counter + "guess to get it right");
+
+        } else {
+            alert("Nope, maybe anther try");
+        }
+        counter++;
     } else {
-        alert("Maybe another guess");
+        // (counter > maxGuess)
+        counter = 0;
+        alert("You have exhausted your available guesses. You can start all over again");
     }
-})
+    if (getInput <= 0) {
+        alert("sorry the number you input is too small or out of range");
+    }
+
+};
+
+// let getCount = document.querySelector("getBttn").onclick = function () {
+//     let val = document.querySelector("h2").innerHTML = counter;
+// };
+
