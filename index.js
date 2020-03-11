4e560b07f3979ef84a1eb065e9a9b008801efbeb
Ola Ishola <olaishola@hotmail.co.uk>
2020-03-11 22:00:40 +0100

    Guessing game final version 1.0

--- index.js
+++ index.js
@@ -7,18 +7,19 @@ const getBtn = document.getElementById("getBttn").onclick = function () {
     if (counter < maxGuess) {
 
         if (getInput == randNum) {
-            alert("Hooray! Congratulation you win!!!!. It took you " + '' + counter + '' + "guess to get it right");
+            alert("Hooray! Congratulation you win!!!!. It took you " + '' + counter + '' + " guess to get it right");
+            counter = 0;
         }
 
-        if (getInput > 10) {
-            alert("Out of range");
+        else if (getInput > 10) {
+            alert("The number you input is Out of range");
         }
-        if (getInput <= 0) {
-            alert("the number u entered is below range");
+        else if (getInput <= 0) {
+            alert("the number you entered is below the range");
         }
 
-        if (getInput != randNum) {
-            alert("Another guess is your bestfriend");
+        else if (getInput != randNum) {
+            alert("Another guess is your is just a click away");
         }
 
         counter++;
@@ -27,39 +28,26 @@ const getBtn = document.getElementById("getBttn").onclick = function () {
         counter = 0;
         alert("You have exhausted your available guesses. You can start all over again");
     }
-
-    // let countGuess = document.getElementById("getBttn").onclick = function () {
-    //     let guessCount = document.getElementById("getInput").value;
-    //     if (guessCount > 10) {
-    //         alert("the number u entered is too large");
-    //     }
-    //     if (guessCount <= 0) {
-    //         alert("sorry the number you input is too small or out of range");
-    //     }
-    // };
 };
 
+document.getElementById("getBttn").addEventListener("click", count);
 
-// countGuess();
-// let counting = document.getElementById("counter").onclick = function () {
-//     count = 0;
+function count() {
+    document.getElementById("count").innerHTML = "Counter: " + counter;
+    if (counter == maxGuess) {
+        counter = 0;
+    }
+}
 
-//     counting.getBtn.innerHTML = "count: " + count;
-//     // let myCounter = document.getElementsByClassName("counter").value;
-//     // myCounter.onclick = function () {
-//     //     count += 1;
-//     //     myCounter.innerHTML = "count: " + count;
-//     // };
-// };
+document.getElementById("getBttn").addEventListener("click", clearInput);
 
 
+function clearInput() {
+    document.getElementById("getInput").value = "";
+}
 
 
 
 
-// function count() {
-//     count = document.getElementsByClassName(".counter");
-//     counter = count.value;
-//     count.innerHTML = counter;
-// }
+
 
